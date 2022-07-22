3d7a7d426eadb84ecf8604b62a06eb1ff48a20c3
Parag Bharadva <parag.b@simformsolutions.com>
2022-07-22 13:02:52 +0530

    Added Whiteboard session configuration

--- client/src/utils/setupWhiteboard.js
+++ client/src/utils/setupWhiteboard.js
@@ -1,11 +1,17 @@
 const setupWhiteboard = (roomId, userId) => {
-    console.log(userId)
-    let pixel = new window.Comet({
-        room: roomId,
-        key: 'vJMI27Iw6tOkDndquXxwCNfskDeW6bLBkK5npSAB',
-        name: userId
-    });
-    console.log(roomId)
+    const isexist = sessionStorage.getItem('whiteboard')
+    console.log(isexist)
+    if(!isexist){
+        console.log("Creating new Whiteboard")
+        sessionStorage.setItem("whiteboard", true)
+        let pixel = new window.Comet({
+            room: roomId,
+            key: 'vJMI27Iw6tOkDndquXxwCNfskDeW6bLBkK5npSAB',
+            name: userId
+        });
+        console.log(pixel)
+    }
+    
 }
 
 export default setupWhiteboard
